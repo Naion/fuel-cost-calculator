330d7bd745f5d748b3ac2cf8d114e47ce83b8cfe
Naion <pachecobravoruben@gmail.com>
2023-05-23 21:50:06 +0200

    implemented car API

--- main.py
+++ main.py
@@ -38,11 +38,45 @@ for country in contents[17:-16]:
         origin = geo.geocode(input('From where are you starting your trip? '))
         destination = geo.geocode(input('Where is your destination? '))
         gas_type = input ('Which fuel do you use? (95/oil/gas) ')
-        ask_consumo = float(input('How many liters does your car consume at 100Km? '))
+        car_info = input ('Which maker and model is your car? ').split(' ')
+        car_maker = car_info[0]
+        car_model = car_info[1]
+        
 
-        consumo = ask_consumo/100
+        car_url = "https://car-utils.p.rapidapi.com/fueleconomy"
 
-        url = "https://distanceto.p.rapidapi.com/get"
+        querystring = {"make":f"{car_maker}",f"{car_model}":"500"}
+
+        headers = {
+            "X-RapidAPI-Key": "0633413946msh2052f9e7d584db6p1595d2jsnd8ee7f654313",
+            "X-RapidAPI-Host": "car-utils.p.rapidapi.com"
+        }
+
+        car_response = requests.get(car_url, headers=headers, params=querystring)
+        json_car = car_response.json()
+
+        if isinstance(json_car, list) == False:
+            if 'not found' in json_car.values():
+                answer = input('Car not found, do you want to put manually the fuel economy? (yes/no) ')
+                if answer == 'yes':
+                    ask_consumo = float(input('How many liters does your car consume at 100Km? '))
+                    consumo = ask_consumo/100
+                elif answer == 'no':
+                    print('Closing program...')
+                    exit()
+                else:
+                    print('Invalid answer')
+                    exit()
+        else:
+            total_mpg = 0
+            lenght = 0
+            for car in json_car:
+                if car['fuel_type'] != 'Electricity':
+                    total_mpg += car['combined_mpg']
+                    lenght += 1
+            consumo = (235.215 / (total_mpg / lenght)) / 100
+
+        trip_url = "https://distanceto.p.rapidapi.com/get"
 
         querystring = {"route":'[{\"t\":\"'+str(origin.latitude)+','+ str(origin.longitude)+'\"},{\"t\":\"'+str(destination.latitude)+','+ str(destination.longitude)+'\"}]',"car":"true"}
 
@@ -52,11 +86,11 @@ for country in contents[17:-16]:
             "X-RapidAPI-Host": "distanceto.p.rapidapi.com"
         }
 
-        trip_response = requests.get(url, headers=headers, params=querystring).text
-        json_text = json.loads(trip_response)
-        car_info = json_text['steps'][0]['distance']['car']
-        car_distance = float(car_info['distance'] / 1000)
-        car_duration = float(car_info['duration'] / 3600)
+        trip_response = requests.get(trip_url, headers=headers, params=querystring).text
+        json_trip = data.json_page
+        car_trip_info = json_trip['steps'][0]['distance']['car']
+        car_distance = float(car_trip_info['distance'] / 1000)
+        car_duration = float(car_trip_info['duration'] / 3600)
 
         min, hour = math.modf(car_duration)
 
